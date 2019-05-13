80beb74b3498cdf8b56586acbd35ff9d9ab583f5
Tim Baek <tim.baek@hotmail.com>
2019-05-13 09:54:34 -0400

    daily-mail: init day 1 prob

---
+++ daily-mail/day1.py
@@ -0,0 +1,31 @@
+"""
+Given an integer array, find the largest consecutive sum of elements.
+
+예제}
+
+Input: [-1, 3, -1, 5]
+
+Output: 7 // 3 + (-1) + 5
+
+
+
+Input: [-5, -3, -1]
+
+Output: -1 // -1
+
+
+
+Input: [2, 4, -2, -3, 8]
+
+Output: 9 // 2 + 4 + (-2) + (-3) + 8
+"""
+def solution(n, arr1, arr2):
+    pass
+
+def main():
+    pass
+    
+    # print(solution(n,arr1,arr2))
+
+if __name__ == '__main__':
+    main()
