ab7a76ba75248640f75e77190156ce37acd31f33
Tim Baek <tim.baek@hotmail.com>
2019-05-13 10:17:26 -0400

    daily-mail: complete day 1 problem

--- daily-mail/day1.py
+++ daily-mail/day1.py
@@ -1,12 +1,14 @@
+from sys import maxint
+
 """
 Given an integer array, find the largest consecutive sum of elements.
+Time Complexity: O(n)
 
-예제}
+Ex}
 
 Input: [-1, 3, -1, 5]
 
 Output: 7 // 3 + (-1) + 5
-
 
 
 Input: [-5, -3, -1]
@@ -14,18 +16,32 @@ Input: [-5, -3, -1]
 Output: -1 // -1
 
 
-
 Input: [2, 4, -2, -3, 8]
 
 Output: 9 // 2 + 4 + (-2) + (-3) + 8
 """
-def solution(n, arr1, arr2):
-    pass
+def solution(arr):
+    max_so_far = -maxint - 1
+    max_ending_here = 0
+
+    for x in arr:
+        max_ending_here = max_ending_here + x
+        if max_so_far < max_ending_here:
+            max_so_far = max_ending_here
+        
+        if max_ending_here < 0:
+            max_ending_here = 0
+    
+    return max_so_far
 
 def main():
-    pass
+    arr1 = [-1, 3, -1, 5]
+    arr2 = [-5, -3, -1]
+    arr3 = [2, 4, -2, -3, 8]
     
-    # print(solution(n,arr1,arr2))
+    print(solution(arr1))
+    print(solution(arr2))
+    print(solution(arr3))
 
 if __name__ == '__main__':
     main()
