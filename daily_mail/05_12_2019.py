82ee7fc8fa8480d66510eb54684c6545de5d53a0
Tim Baek <tim.baek@hotmail.com>
2019-09-30 17:53:10 -0400

    re init

---
+++ daily_mail/05_12_2019.py
@@ -0,0 +1,26 @@
+'''
+Given an integer N, find the number of possible balanced parentheses with N opening and closing brackets.
+
+예제)
+
+Input: 1
+Output: ["()"]
+
+Input: 2
+Output: ["(())", "()()"]
+
+Input: 3
+Output: ["((()))", "(()())", "()(())", "(())()", "()()()"]
+'''
+
+def solution(n):
+  pass
+
+
+output_1 = ["()"]
+output_2 = ["(())", "()()"]
+output_3 = ["((()))", "(()())", "()(())", "(())()", "()()()"]
+
+assert output_1 == solution(1)
+assert output_2 == solution(2)
+assert output_3 == solution(3)
